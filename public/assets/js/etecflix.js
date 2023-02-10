99118332449b1b9a38363301428736ef6fc62fdb
unknown <hugo27oliveira@hotmail.com>
2023-02-10 20:42:31 -0300

    Login com Firebase

---
+++ public/assets/js/etecflix.js
@@ -0,0 +1,72 @@
+import cidades from '../dados/cidades.json' assert {type:'json'};
+import endereco from '../dados/enderoco.json' assert {type:'json'};
+
+
+function popularCategoria()
+{
+    let divMenu = document.getElementById('menu');
+
+    for (let idx = 0; idx < cidades.length; idx++) 
+    {
+        let a = document.createElement('a');
+        a.id ='link' + cidades[idx].id ;
+        a.className = 'flex-menu-link';
+        a.innerText = cidades[idx].descricao;
+
+        a.addEventListener('click', () => exibirEndereco(cidades[idx].id));
+        divMenu.appendChild(a);
+    }
+    if(cidades.length > 0)
+    {
+        exibirEndereco(cidades[0].id);
+    }             
+}
+
+function exibirEndereco(cidadeId) 
+{ 
+    let cidadeSelecionada = document.getElementsByClassName('selecionado');
+    if (cidadeSelecionada.length > 0) 
+    {
+        cidadeSelecionada[0].className = 'flex-menu-link';
+    }
+
+
+    let novaCidadeSelecionada = document.getElementById('link' + cidadeId);
+    novaCidadeSelecionada.className  += ' selecionado';
+
+    let locais = endereco.filter(local =>{
+        return local.cidadeId == cidadeId;
+    });
+
+    let divEndereco = document.getElementById('catalogo');
+    divEndereco.innerHTML = '';
+
+    locais.forEach(local=> {
+
+        let divCard = document.createElement('div');
+        divCard.className = 'flex-catalogo-card';
+        
+
+        let h4NomeLocalC = document.createElement('h4');
+        h4NomeLocalC.innerText = local.descricao; + '<br/>';
+        h4NomeLocalC.className = 'nome-localC';
+        h4NomeLocalC.title = local.descricao;
+
+        let h4Numero = document.createElement('h4');
+        h4Numero.innerText = local.numero;
+
+
+        divCard.appendChild(h4NomeLocalC);
+        divCard.appendChild(h4Numero);
+
+        divEndereco.appendChild(divCard);
+
+    });
+
+}
+
+function inicializar(){
+    popularCategoria();
+}
+
+inicializar();
---
+++ public/assets/js/index.js
@@ -0,0 +1,36 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
+import { getAuth, signInWithEmailAndPassword  } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-auth.js";
+//#region Configuração do App Key
+const firebaseConfig = {
+  apiKey: "AIzaSyCIewhvwhvbabisNkBjYWHtSgRnZh49aog",
+  authDomain: "ecicloo.firebaseapp.com",
+  projectId: "ecicloo",
+  storageBucket: "ecicloo.appspot.com",
+  messagingSenderId: "621707276043",
+  appId: "1:621707276043:web:d18d697c865bff8b187c0e"
+};
+const app = initializeApp(firebaseConfig);
+//#endregion
+
+//#region Area de Login Firebase
+const auth = getAuth(app);
+
+const btSign = document.getElementById("btSign");
+var email = "";
+var password = "";
+btSign.addEventListener("click", async(err) => {
+  email = document.getElementById("email").value;
+  password = document.getElementById("password").value;
+  console.log(email + "|" + password);
+  signInWithEmailAndPassword(auth, email, password)
+  .then((userCredential) => {
+    alert("Ambiente Logado no Firebase")
+    const user = userCredential.user;
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    alert(errorCode + " | " + "Deu Rum não existe no Firebase.")
+  });
+});
+
