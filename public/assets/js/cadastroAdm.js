cdbef023f2deca24f3a0ec7611d71a73a6afb1f1
Vinicius Alcides <vinicius.alcides03@gmail.com>
2023-03-02 16:46:19 -0300

    atualização tela adm

---
+++ public/assets/js/cadastroAdm.js
@@ -0,0 +1,17 @@
+import cidades from '../dados/cidades.json' assert {type:'json'};
+
+let botaoConfirmar = document.getElementById('botaoConfirma');
+botaoConfirmar.addEventListener('click', () => Confirmar());
+
+function Confirmar(){
+    let inputIdcidade = document.getElementById('idCidade').value;
+    let inputCidade = document.getElementById('cidade').value;
+
+    let objCategoria = {
+        id: inputIdcidade,
+        descricao: inputCidade
+    }
+
+    cidades.push(objCategoria);
+    console.log(cidades);
+}
