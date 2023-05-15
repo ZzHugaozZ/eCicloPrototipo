c2d79b27db0a538b7108c5654adcfb3987f4e3f0
Aluno <aluno@etec.sp.gov.br>
2023-05-15 20:38:55 -0300

    rota de adm

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,6 +1,7 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
 import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-auth.js";
 import {  sendPasswordResetEmail } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-auth.js";
+import { getFirestore, getDocs, collection, query, where} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-firestore.js";
 
 // var admin = require("https://www.gstatic.com/firebasejs/9.15.0/firebase-admin.js");
 
@@ -34,9 +35,11 @@ btSign.addEventListener("click", async(err) => {
   password = document.getElementById("password").value;
   signInWithEmailAndPassword(auth, email, password)
   .then((userCredential) => {
+    console.log(userCredential);
+    validationUser(auth);
     alert("Ambiente Logado no Firebase")
+    debugger;
     window.location.href = "../telas/tInicial.html"
-    const user = userCredential.user;
   })
   .catch((error) => {
     const errorCode = error.code;
@@ -70,9 +73,6 @@ btnRSenha.addEventListener("click", (error) => {
   recoverPassword();
  })
 //#endregion
-
-onAuthStateChanged(auth, (user) => {
-  if (user) {
-    window.location.href = "../../telas/tAdm.html";
-  }
-});
+function validationUser(user){
+  console.log(user)
+}
